8753d2fd26f06e25591d86497793672d911822ba
Christopher Reinartz <ccrein@elektro.dtu.dk>
2021-04-07 20:30:40 +0200

    Updated use case examples and gitignore.

--- use_examples.py
+++ use_examples.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 tep = SimInterface.setup()
 
-save_dir = Path("./save_scenarios").absolute()
+save_dir = Path("./example_save").absolute()
 durations = [0, 10, 20, 30, 40]
 gains = [0.85, 0.9, 0.95, 1, 1.05, 1.1, 1.15]
 for sp in tep.setpoint_labels:
@@ -14,27 +14,23 @@ for sp in tep.setpoint_labels:
         tep.reset()
         tep.ramp_setpoint(sp, sp_target,
                           duration=d, delay=30)
-        tep.simulate(duration=1)
+        tep.simulate(duration=100)
         tep.save_all(save_dir)
 
 
-### Example number two (showcasing the interactive capabilities of pytep - not possible with any other simulator rn)
+## Example number two (showcasing the interactive capabilities of pytep - not possible with any other simulator rn)
 
-# from pytep.siminterface import SimInterface
-# from planning.greedy_tep_planner import GreedyTEPPlanner
-# 
-# tep = SimInterface.setup()
-# tep.setup()
-# planner = GreedyTEPPlanner()
-# save_dir = "./example_save_dir"
-# 
-# planning_complete = False
-# current_tep_state = tep.latest_process_data()
-# while not planner.goal_reached(current_tep_state):
-#     best_action = planner.plan(current_tep_state)
-#     tep.ramp_setpoint(**best_action)
-#     tep.extend_simulation(1)
-#     tep.simulate()
-#     current_tep_state = tep.latest_process_data()
-#     tep.log_process_data()
-# tep.save(save_dir)
+from pytep.siminterface import SimInterface
+from planning.tep_planner import TEPPlanner
+
+tep = SimInterface.setup()
+plnnr = TEPPlanner()
+
+save_dir = Path("./example_save").absolute()
+tep_state = tep.current_process_data()
+while not plnnr.goal_reached(tep_state):
+    best_action = plnnr.plan(tep_state)
+    tep.ramp_setpoint(**best_action)
+    tep.simulate(1)
+    tep_state = tep.current_process_data()
+tep.save_all(save_dir)
