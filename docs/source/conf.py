66772a237aae128142b4c8e766cda8284b4c7dc8
Christopher Reinartz <ccrein@elektro.dtu.dk>
2021-04-13 01:27:12 +0200

    Docs improvement.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ html_static_path = ['_static']
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath(os.path.join('..','..'))
 
 github_doc_root = 'https://github.com/ChristopherReinartz/pytep/tree/master/docs'
 sys.path.insert(0, os.path.abspath(os.path.join(github_doc_root, '..','..')))
